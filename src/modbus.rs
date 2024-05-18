2fbff5e0cc77110ab105dfec359f9819aab75b02
David Loewe <49597367+TumbleOwlee@users.noreply.github.com>
2024-05-18 03:09:26 +0200

    Remove outdated code

--- src/modbus.rs
+++ src/modbus.rs
@@ -1,17 +1,10 @@
 use crate::memory::{Memory, Range};
 
 use std::{
-    collections::HashMap,
     future,
-    net::SocketAddr,
     sync::{Arc, Mutex},
-    time::Duration,
 };
-use tokio::net::TcpListener;
-use tokio_modbus::{
-    prelude::*,
-    server::tcp::{accept_tcp_connection, Server as TcpServer},
-};
+use tokio_modbus::prelude::{Exception, Request, Response};
 
 pub struct Server<const SLICE_SIZE: usize> {
     memory: Arc<Mutex<Memory<SLICE_SIZE, u16>>>,
@@ -65,43 +58,3 @@ impl<const SLICE_SIZE: usize> Server<SLICE_SIZE> {
         Self { memory }
     }
 }
-
-//async fn client_context(socket_addr: SocketAddr) {
-//    tokio::join!(
-//        async {
-//            // Give the server some time for starting up
-//            tokio::time::sleep(Duration::from_secs(1)).await;
-//
-//            println!("CLIENT: Connecting client...");
-//            let mut ctx = tcp::connect(socket_addr).await.unwrap();
-//
-//            println!("CLIENT: Reading 2 input registers...");
-//            let response = ctx.read_input_registers(0x00, 2).await.unwrap();
-//            println!("CLIENT: The result is '{response:?}'");
-//            assert_eq!(response.unwrap(), vec![1234, 5678]);
-//
-//            println!("CLIENT: Writing 2 holding registers...");
-//            ctx.write_multiple_registers(0x01, &[7777, 8888])
-//                .await
-//                .unwrap()
-//                .unwrap();
-//
-//            // Read back a block including the two registers we wrote.
-//            println!("CLIENT: Reading 4 holding registers...");
-//            let response = ctx.read_holding_registers(0x00, 4).await.unwrap();
-//            println!("CLIENT: The result is '{response:?}'");
-//            assert_eq!(response.unwrap(), vec![10, 7777, 8888, 40]);
-//
-//            // Now we try to read with an invalid register address.
-//            // This should return a Modbus exception response with the code
-//            // IllegalDataAddress.
-//            println!("CLIENT: Reading nonexistent holding register address... (should return IllegalDataAddress)");
-//            let response = ctx.read_holding_registers(0x100, 1).await.unwrap();
-//            println!("CLIENT: The result is '{response:?}'");
-//            assert!(matches!(response, Err(Exception::IllegalDataAddress)));
-//
-//            println!("CLIENT: Done.")
-//        },
-//        tokio::time::sleep(Duration::from_secs(5))
-//    );
-//}
